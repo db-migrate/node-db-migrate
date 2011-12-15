2d66e9de7d0b3bc08868ec7b8fa82f57200593f2
Joe Ferner <joe@fernsroth.com>
2011-12-15 11:43:20 -0500

    support calling migrations not on the command line

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+
+var config = require('./lib/config');
+var Migrator = require('./lib/migrator');
 exports.dataType = require('./lib/data_type');
+
+exports.connect = function(options, callback) {
+  config['__connect'] = options;
+  config.setCurrent('__connect');
+  var env = config.getCurrent();
+  var driver = require('./lib/driver');
+  driver.connect(env, function(err, db) {
+    if (err) { callback(err); return; }
+    db.createMigrationsTable(function(err) {
+      if (err) { callback(err); return; }
+      callback(null, new Migrator(db));
+    });
+  });
+}
