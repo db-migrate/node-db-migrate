e8ce111a6165f4dd48ea836ed06f7ee2a40e20a5
Igor Zinovyev <zinigor@gmail.com>
2013-12-06 00:23:36 +0400

    Removed temporary credentials from the spec.

--- test/driver/pg_test.js
+++ test/driver/pg_test.js
@@ -4,7 +4,7 @@ var dbmeta = require('db-meta');
 var dataType = require('../../lib/data_type');
 var driver = require('../../lib/driver');
 
-driver.connect({ driver: 'pg', database: 'db_migrate_test', user: 'dbmig', password: 'dbmig' }, function(err, db) {
+driver.connect({ driver: 'pg', database: 'db_migrate_test' }, function(err, db) {
   vows.describe('pg').addBatch({
     'createTable': {
       topic: function() {
