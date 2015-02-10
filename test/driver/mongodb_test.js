7dcc162cb703e66e63a095f8e9fc4d8e8b9fbd6a
Tom Caflisch <tomcaflisch@gmail.com>
2015-02-10 14:37:03 -0600

    Fixed formatting issues

--- test/driver/mongodb_test.js
+++ test/driver/mongodb_test.js
@@ -8,281 +8,280 @@ var config = require('../db.config.json').mongodb;
 
 var dbName = config.database;
 driver.connect(config, function(err, db) {
-	assert.isNull(err);
+  assert.isNull(err);
   vows.describe('mongodb')
   .addBatch({
-		'createCollection': {
-			topic: function() {
-				db.createCollection('event', this.callback);
-			},
-  
-			teardown: function() {
-				db.dropCollection('event', this.callback);
-			},
+    'createCollection': {
+      topic: function() {
+        db.createCollection('event', this.callback);
+      },
+
+      teardown: function() {
+        db.dropCollection('event', this.callback);
+      },
         
-			'has table metadata': {
-				topic: function() {					
-					db._getCollectionNames(this.callback);
-				},
-  
-				'containing the event table': function(err, tables) {
-					assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
-				}
-			} 
-		}
-	}) 
+      'has table metadata': {
+        topic: function() {
+          db._getCollectionNames(this.callback);
+        },
+
+        'containing the event table': function(err, tables) {
+          assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
+        }
+      } 
+    }
+  }) 
   .addBatch({
- 		'dropCollection': {
- 			topic: function() {
- 				db.createCollection('event', function(err, collection) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 					
- 					db.dropCollection('event', this.callback);
- 				}.bind(this));
- 			},
-       
- 			'has table metadata': {
- 				topic: function() {
- 					db._getCollectionNames(this.callback);
- 				},
-       
- 				'containing no tables': function(err, tables) {
- 					assert.isNotNull(tables);
- 					assert.equal(tables.length, 1);	// Should be 1 b/c of the system collection
- 				}
- 			}
- 		} 
- 	}) 
- 	.addBatch({
- 		'renameCollection': {
- 			topic: function() {
- 				db.createCollection('event', function(err, collection) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 					
- 					db.renameCollection('event', 'functions', this.callback);
- 				}.bind(this));
- 			},
-            
- 			teardown: function() {
- 				db.dropCollection('functions', this.callback);
- 			},
-             
-       
- 			'has table metadata': {
- 				topic: function() {
- 					db._getCollectionNames(this.callback);
- 				},
-       
- 				'containing the functions table': function(err, tables) {
- 					assert.isNotNull(tables);
- 					assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
- 					assert.equal(tables[1].collectionName, 'functions');
- 				}
- 			} 
- 		}
- 	})
- 	.addBatch({
- 		'addIndex': {
- 			topic: function() {
- 				db.createCollection('event', function(err, collection) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 					
- 					db.addIndex('event', 'event_title', 'title', false, this.callback);
- 				}.bind(this));    
- 			},
-       
- 			teardown: function() {
- 				db.dropCollection('event', this.callback);
- 			},
-            						 
- 			'preserves case': {
- 				topic: function() {
- 					db._getCollectionNames(this.callback);
- 				},
-       
- 				'of the functions original table': function(err, tables) {
- 					assert.isNotNull(tables);
- 					assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
- 					assert.equal(tables[1].collectionName, 'event');
- 				}
- 			},
-       
- 			'has resulting index metadata': {
- 				topic: function() {
- 					db._getIndexes('event', this.callback);
- 				},
-       
- 				'with additional index': function(err, indexes) {
- 					assert.isDefined(indexes);
- 					assert.isNotNull(indexes);
- 					assert.include(indexes, 'event_title');
- 				}
- 			}
- 		} 
- 	})
- 	.addBatch({
- 		'insert': {
- 			topic: function() {
- 				db.createCollection('event', function(err, collection) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 					db.insert('event', [{id: 2, title: 'title'}], this.callback);
- 				}.bind(this));
- 			},
-       
- 			teardown: function() {
- 				db.dropCollection('event', this.callback);
- 			},
-             
- 			'with additional row' : function() {
- 				db._find('event', {title: 'title'}, function(err, data) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 								
- 					assert.equal(data.length, 1);
- 				});
- 			}
- 		} 
- 	})
- 	.addBatch({
- 		'removeIndex': {
- 			topic: function() {
- 				db.createCollection('event', function(err, collection) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 								
- 					db.addIndex('event', 'event_title', 'title', false, function(err, data) {
- 																	
- 						if(err) {
- 							return this.callback(err);
- 						}
- 																	
- 						db.removeIndex('event', 'event_title', this.callback);
- 					}.bind(this));        
- 				}.bind(this));
- 			},
-             
- 			teardown: function() {
- 				db.dropCollection('event', this.callback);
- 			},
-             
-       
- 			'has resulting index metadata': {
- 				topic: function() {
- 					db._getIndexes('event', this.callback);
- 				},
-       
- 				'without index': function(err, indexes) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 								
- 					assert.isDefined(indexes);
- 					assert.isNotNull(indexes);
- 					assert.notInclude(indexes, 'event_title');
- 				}
- 			}
- 		} 
- 	})
- 	.addBatch({
- 		'createMigrationsTable': {
- 			topic: function() {
- 				db._createMigrationsCollection(this.callback);
- 			},
-       
- 			teardown: function() {
- 				db.dropCollection('migrations', this.callback);
- 			},
- 
- 			'has migrations table': {
- 				topic: function() {
- 					db._getCollectionNames(this.callback);
- 				},
-       
- 				'has migrations table' : function(err, tables) {
- 					assert.isNull(err);
- 					assert.isNotNull(tables);
- 					assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
- 					assert.equal(tables[1].collectionName, 'migrations');
- 				}
- 			}
- 		} 
- 	})
- 	.addBatch({
- 		'removeIndex': {
- 			topic: function() {
- 				db.createCollection('event', function(err, collection) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 								
- 					db.addIndex('event', 'event_title', 'title', false, function(err, data) {
- 																	
- 						if(err) {
- 							return this.callback(err);
- 						}
- 																	
- 						db.removeIndex('event', 'event_title', this.callback);
- 					}.bind(this));        
- 				}.bind(this));
- 			},
-             
- 			teardown: function() {
- 				db.dropCollection('event', this.callback);
- 			},
-             
-       
- 			'has resulting index metadata': {
- 				topic: function() {
- 					db._getIndexes('event', this.callback);
- 				},
-       
- 				'without index': function(err, indexes) {
- 					if(err) {
- 						return this.callback(err);
- 					}
- 								
- 					assert.isDefined(indexes);
- 					assert.isNotNull(indexes);
- 					assert.notInclude(indexes, 'event_title');
- 				}
- 			}
- 		} 
- 	})
- 	.addBatch({
- 		'createMigrationsTable': {
- 			topic: function() {
- 				db._createMigrationsCollection(this.callback);
- 			},
-       
- 			teardown: function() {
- 				db.dropCollection('migrations', this.callback);
- 			},
- 
- 			'has migrations table': {
- 				topic: function() {
- 					db._getCollectionNames(this.callback);
- 				},
-       
- 				'has migrations table' : function(err, tables) {
- 					assert.isNull(err);
- 					assert.isNotNull(tables);
- 					assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
- 					assert.equal(tables[1].collectionName, 'migrations');
- 				}
- 			}
- 		} 
- 	
- 	})
+    'dropCollection': {
+      topic: function() {
+        db.createCollection('event', function(err, collection) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          db.dropCollection('event', this.callback);
+        }.bind(this));
+      },
+
+      'has table metadata': {
+        topic: function() {
+          db._getCollectionNames(this.callback);
+        },
+
+        'containing no tables': function(err, tables) {
+          assert.isNotNull(tables);
+          assert.equal(tables.length, 1);	// Should be 1 b/c of the system collection
+        }
+      }
+    }
+  }) 
+  .addBatch({
+    'renameCollection': {
+      topic: function() {
+        db.createCollection('event', function(err, collection) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          db.renameCollection('event', 'functions', this.callback);
+        }.bind(this));
+      },
+
+      teardown: function() {
+        db.dropCollection('functions', this.callback);
+      },
+
+
+      'has table metadata': {
+        topic: function() {
+          db._getCollectionNames(this.callback);
+        },
+
+        'containing the functions table': function(err, tables) {
+          assert.isNotNull(tables);
+          assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
+          assert.equal(tables[1].collectionName, 'functions');
+        }
+      }
+    }
+  })
+  .addBatch({
+    'addIndex': {
+      topic: function() {
+        db.createCollection('event', function(err, collection) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          db.addIndex('event', 'event_title', 'title', false, this.callback);
+        }.bind(this));    
+      },
+
+      teardown: function() {
+        db.dropCollection('event', this.callback);
+      },
+
+      'preserves case': {
+        topic: function() {
+          db._getCollectionNames(this.callback);
+        },
+
+        'of the functions original table': function(err, tables) {
+          assert.isNotNull(tables);
+          assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
+          assert.equal(tables[1].collectionName, 'event');
+        }
+      },
+
+      'has resulting index metadata': {
+        topic: function() {
+          db._getIndexes('event', this.callback);
+        },
+
+        'with additional index': function(err, indexes) {
+          assert.isDefined(indexes);
+          assert.isNotNull(indexes);
+          assert.include(indexes, 'event_title');
+        }
+      }
+    }
+  })
+  .addBatch({
+    'insert': {
+      topic: function() {
+        db.createCollection('event', function(err, collection) {
+          if(err) {
+            return this.callback(err);
+          }
+          db.insert('event', [{id: 2, title: 'title'}], this.callback);
+        }.bind(this));
+      },
+
+      teardown: function() {
+        db.dropCollection('event', this.callback);
+      },
+
+      'with additional row' : function() {
+        db._find('event', {title: 'title'}, function(err, data) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          assert.equal(data.length, 1);
+        });
+      }
+    }
+  })
+  .addBatch({
+    'removeIndex': {
+      topic: function() {
+        db.createCollection('event', function(err, collection) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          db.addIndex('event', 'event_title', 'title', false, function(err, data) {
+
+            if(err) {
+              return this.callback(err);
+            }
+
+            db.removeIndex('event', 'event_title', this.callback);
+          }.bind(this));
+        }.bind(this));
+      },
+
+      teardown: function() {
+        db.dropCollection('event', this.callback);
+      },
+
+
+      'has resulting index metadata': {
+        topic: function() {
+          db._getIndexes('event', this.callback);
+        },
+
+        'without index': function(err, indexes) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          assert.isDefined(indexes);
+          assert.isNotNull(indexes);
+          assert.notInclude(indexes, 'event_title');
+        }
+      }
+    }
+  })
+  .addBatch({
+    'createMigrationsTable': {
+      topic: function() {
+        db._createMigrationsCollection(this.callback);
+      },
+
+      teardown: function() {
+        db.dropCollection('migrations', this.callback);
+      },
+
+      'has migrations table': {
+        topic: function() {
+          db._getCollectionNames(this.callback);
+        },
+
+        'has migrations table' : function(err, tables) {
+          assert.isNull(err);
+          assert.isNotNull(tables);
+          assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
+          assert.equal(tables[1].collectionName, 'migrations');
+        }
+      }
+    }
+  })
+  .addBatch({
+    'removeIndex': {
+      topic: function() {
+        db.createCollection('event', function(err, collection) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          db.addIndex('event', 'event_title', 'title', false, function(err, data) {
+
+            if(err) {
+              return this.callback(err);
+            }
+
+            db.removeIndex('event', 'event_title', this.callback);
+          }.bind(this));
+        }.bind(this));
+      },
+
+      teardown: function() {
+        db.dropCollection('event', this.callback);
+      },
+
+
+      'has resulting index metadata': {
+        topic: function() {
+          db._getIndexes('event', this.callback);
+        },
+
+        'without index': function(err, indexes) {
+          if(err) {
+            return this.callback(err);
+          }
+
+          assert.isDefined(indexes);
+          assert.isNotNull(indexes);
+          assert.notInclude(indexes, 'event_title');
+        }
+      }
+    }
+  })
+  .addBatch({
+    'createMigrationsTable': {
+      topic: function() {
+        db._createMigrationsCollection(this.callback);
+      },
+
+      teardown: function() {
+        db.dropCollection('migrations', this.callback);
+      },
+
+      'has migrations table': {
+        topic: function() {
+          db._getCollectionNames(this.callback);
+        },
+
+        'has migrations table' : function(err, tables) {
+          assert.isNull(err);
+          assert.isNotNull(tables);
+          assert.equal(tables.length, 2);	// Should be 2 b/c of the system collection
+          assert.equal(tables[1].collectionName, 'migrations');
+        }
+      }
+    }
+  })
  .export(module);
 });
 
