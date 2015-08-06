741aeb1ba95ff131594f297fea784501833c4e0b
Mike Atkins <mike.atkins@lanetix.com>
2015-08-06 17:48:16 -0400

    port changes from merge request #287

--- test/driver/pg_schema_test.js
+++ test/driver/pg_schema_test.js
@@ -1,4 +1,5 @@
 var vows = require('vows');
+var Promise = require('bluebird');
 var assert = require('assert');
 var dbmeta = require('db-meta');
 var pg = require('pg');
@@ -55,6 +56,67 @@ vows.describe('pg').addBatch({
     }
 })
 .addBatch({
+    'create schema and a public.migrations table and connect': {
+        topic: function() {
+            var callback = this.callback;
+            var client = new pg.Client(databaseUrl);
+            var query = Promise.promisify( client.query ).bind(client);
+
+            client.connect(function (err) {
+                if (err) { return callback(err) };
+                Promise.all([
+                  query('CREATE SCHEMA test_schema'),
+                  query('CREATE TABLE migrations ()')
+                ])
+                .then( function() {
+
+                    driver.connect({ driver: 'pg', database: 'db_migrate_test', schema: 'test_schema' }, internals, function(err, db) {
+                        callback(err, db, client);
+                    });
+                })
+                .catch( function(err) {
+                  callback(err);
+                });
+            });
+        },
+
+        'migrations table': {
+            topic: function(db, client) {
+                var callback = this.callback;
+
+                db.createMigrationsTable(function() {
+                    client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'test_schema' AND table_name = 'migrations'", function(err, result) {
+                        callback(err, result);
+                    });
+                });
+            },
+
+            'is in test_schema': function(err, result) {
+                assert.isNull(err);
+                assert.isNotNull(result);
+                assert.equal(result.rowCount, 1);
+            }
+        },
+
+        teardown: function(db, client) {
+            var callback = this.callback;
+            var query = Promise.promisify(client.query).bind(client);
+
+            Promise.all([
+              query('DROP SCHEMA test_schema CASCADE'),
+              query('DROP TABLE migrations')
+            ])
+            .then(function (err) {
+              client.end();
+              callback();
+            })
+            .catch(function (err) {
+              callback(err);
+            });
+        }
+    }
+})
+.addBatch({
     'create schema and connect': {
         topic: function() {
             var callback = this.callback;
@@ -98,4 +160,3 @@ vows.describe('pg').addBatch({
         }
     }
 }).export(module);
-
