489d498bbfc75e0f4f533782f30584e25eba75db
Tobias Gurtzick <magic@wizardtales.com>
2017-10-29 23:59:46 +0100

    complete refactor of api js

--- index.js
+++ index.js
@@ -5,41 +5,33 @@ var path = require('path');
 exports.dataType = require('db-migrate-shared').dataType;
 
 function loadPluginList() {
-
-  var plugins = JSON.parse(fs.readFileSync(
-          path.join(process.cwd(), 'package.json'),
-          'utf-8'
-        )
-      ),
-      targets = [];
+  var plugins = JSON.parse(
+      fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf-8')
+    ),
+    targets = [];
 
   plugins = Object.assign(plugins.dependencies, plugins.devDependencies);
 
-  for(var plugin in plugins) {
-
-    if(plugin.startsWith('db-migrate-plugin'))
-      targets.push(plugin);
+  for (var plugin in plugins) {
+    if (plugin.startsWith('db-migrate-plugin')) targets.push(plugin);
   }
 
   return targets;
 }
 
 function loadPlugins() {
-
   var plugins = loadPluginList(),
-      i = 0,
-      length = plugins.length,
-      hooks = {};
+    i = 0,
+    length = plugins.length,
+    hooks = {};
 
-  for(; i < length; ++i) {
-
+  for (; i < length; ++i) {
     var plugin = require(path.join(process.cwd(), 'node_modules', plugins[i]));
 
-    if(typeof(plugin.name) !== 'string' || !plugin.hooks || !plugin.loadPlugin)
+    if (typeof plugin.name !== 'string' || !plugin.hooks || !plugin.loadPlugin)
       continue;
 
     plugin.hooks.map(function(hook) {
-
       hooks[hook] = hooks[hook] || [];
       hooks[hook].push(plugin);
     });
@@ -49,21 +41,16 @@ function loadPlugins() {
 }
 
 module.exports.getInstance = function(isModule, options, callback) {
-
   delete require.cache[require.resolve('./api.js')];
   delete require.cache[require.resolve('optimist')];
   var mod = require('./api.js'),
-      plugins = {};
+    plugins = {};
 
   try {
+    if (!options || !options.noPlugins) plugins = loadPlugins();
+  } catch (ex) {}
 
-    if(!options || !options.noPlugins)
-      plugins = loadPlugins();
-  }
-  catch(ex) {}
-
-  if(options && options.plugins) {
-
+  if (options && options.plugins) {
     plugins = Object.assign(plugins, options.plugins);
   }
 
