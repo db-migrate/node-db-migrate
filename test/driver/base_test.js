72fce59be67e9259aa5d5bcf4ac5f5a09ad8c817
Tobias Gurtzick <magic@wizardtales.com>
2017-11-22 23:50:25 +0100

    remove parallel: true as it was deprecated

--- test/driver/base_test.js
+++ test/driver/base_test.js
@@ -1,6 +1,6 @@
 var Code = require('code');
 var Lab = require('lab');
-var lab = exports.lab = Lab.script();
+var lab = (exports.lab = Lab.script());
 var Base = require('db-migrate-base');
 
 var internals = {
@@ -11,80 +11,80 @@ var internals = {
   }
 };
 
-lab.experiment('base', { parallel: true }, function () {
-  lab.experiment('default implementation', { parallel: true }, function () {
+lab.experiment('base', function () {
+  lab.experiment('default implementation', function () {
     var base = new Base(internals);
 
-    lab.test('inherits from EventEmitter', { parallel: true }, function (done) {
+    lab.test('inherits from EventEmitter', function (done) {
       Code.expect(base.on).to.be.not.null();
       Code.expect(base.emit).to.be.not.null();
       done();
     });
 
-    lab.test('throws errors for all API methods', { parallel: true },
-      function (done) {
-        Code.expect(function () {
-          base.createTable();
-        }).to.throw(Error);
+    lab.test('throws errors for all API methods', function (done) {
+      Code.expect(function () {
+        base.createTable();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.dropTable();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.dropTable();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.addColumn();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.addColumn();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.removeColumn();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.removeColumn();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.renameColumn();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.renameColumn();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.changeColumn();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.changeColumn();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.addIndex();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.addIndex();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.insert();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.insert();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.removeIndex();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.removeIndex();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.addAssociation();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.addAssociation();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.removeAssociation();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.removeAssociation();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.addForeignKey();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.addForeignKey();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.removeForeignKey();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.removeForeignKey();
+      }).to.throw(Error);
 
-        Code.expect(function () {
-          base.runSql();
-        }).to.throw(Error);
+      Code.expect(function () {
+        base.runSql();
+      }).to.throw(Error);
 
-        done();
-      });
+      done();
+    });
 
-    lab.test('escapes single quotes', { parallel: true }, function (done) {
-      Code.expect('Bill\'\'s Mother\'\'s House')
-        .to.equal(base.escape('Bill\'s Mother\'s House'));
+    lab.test('escapes single quotes', function (done) {
+      Code.expect("Bill''s Mother''s House").to.equal(
+        base.escape("Bill's Mother's House")
+      );
       done();
     });
   });
