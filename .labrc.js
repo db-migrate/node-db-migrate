1d896052a65856e1ced98fcd033ee1f73c4c87f0
Matthew Peveler <matt.peveler@gmail.com>
2022-06-15 11:23:58 -0400

    Replace travis-ci with gh actions
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- .labrc.js
+++ .labrc.js
@@ -3,7 +3,7 @@ module.exports = {
   threshold: 74,
   lint: false,
   globals:
-    'verbose,dryRun,SharedArrayBuffer,Atomics,BigUint64Array,BigInt64Array,BigInt,URL,URLSearchParams,TextEncoder,TextDecoder,queueMicrotask',
+    'verbose,dryRun,SharedArrayBuffer,Atomics,BigUint64Array,BigInt64Array,BigInt,URL,URLSearchParams,TextEncoder,TextDecoder,queueMicrotask,AggregateError,FinalizationRegistry,WeakRef,atob,btoa,AbortController,AbortSignal,EventTarget,Event,MessageChannel,MessagePort,MessageEvent,performance,Blob,BroadcastChannel,structuredClone,DOMException,ReadableStream,ReadableStreamDefaultReader,ReadableStreamBYOBReader,ReadableStreamBYOBRequest,ReadableByteStreamController,ReadableStreamDefaultController,TransformStream,TransformStreamDefaultController,WritableStream,WritableStreamDefaultWriter,WritableStreamDefaultController,ByteLengthQueuingStrategy,CountQueuingStrategy,TextEncoderStream,TextDecoderStream,CompressionStream,DecompressionStream,fetch,FormData,Headers,Request,Response',
   assert: '@hapi/code',
   verbose: true,
   'coverage-exclude': ['lib/interface']
